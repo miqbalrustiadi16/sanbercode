cc49327f947540fd2efc64079a593d165de8704e
Moctar Afendi <moctarafendi@gmail.com>
2025-08-29 23:18:47 +0700

    Add Cypress setup with POM, test cases, and fixtures

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    supportFile: 'cypress/support/e2e.js',
   },
-};
+});
